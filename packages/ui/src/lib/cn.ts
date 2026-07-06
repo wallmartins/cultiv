de562a219c79fd18c481e92aded6ac9fe338dfee
wallmartins <wall_martins_@hotmail.com>
2026-07-06 11:45:34 -0300

    refactor: remove ui package

--- packages/ui/src/lib/cn.ts
+++
@@ -1,5 +0,0 @@
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ReadonlyArray<string | false | undefined>): string {
-  return twMerge(inputs.filter(Boolean).join(" "));
-}
--- packages/ui/src/tokens/spacing.ts
+++
@@ -1,5 +0,0 @@
-export const spacingTokens = {
-  sectionY: "4rem",
-  containerMax: "72rem",
-  gutter: "1.5rem"
-} as const;
